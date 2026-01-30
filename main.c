c93b7fa355ca27f494177054a4965bbabf94c80b
Avidan27 <avidanbhujbal@gmail.com>
2026-01-30 16:34:58 +0000

    updates final

--- main.c
+++ main.c
@@ -1,4 +1,22 @@
-#include <studio.h>
+#include <stdio.h>
 int main(){
-printf('hello world')
+printf("hello world this ic c and it thing single doutes are chars \n ");
+printf("Enter 3 numbers for array one \n ");
+int i;
+int a[3];
+int b[3];
+int c[3];
+for(i=0;i<3;i++){
+    scanf("%d",&a[i]);
+   
+}
+printf("enter 3 numbers for second array \n ");
+for(i=0;i<3;i++){
+    scanf("%d",&b[i]);
+    c[i]=a[i]+b[i];
+}
+for(i=0;i<3;i++){
+printf("%d  ",c[i]);
+}
+ 
 }
