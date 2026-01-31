641b5c3b08bbadd29fad364d61302edfba776e8a
Avidan27 <avidanbhujbal@gmail.com>
2026-01-31 07:07:01 +0000

    tictac

--- p1.py
+++ p1.py
@@ -1,10 +1,10 @@
 print("hello world")
-print("Player 1: enter a number from 1-9 to draw an X \n");
-a=input()
-s='''|  |  |  |
-     |  |  |  |
-     |  |  |  |'''
 
-print(s)
-c=[]
+for i in range(9):
+    print("Player 1: enter a number from 1-9 to draw an X \n");
+    a=int(input())
+    print("player 1 chose box "+a)
+    print("Player 2: enter a number from 1-9 to draw an O \n");
+    b=input()
+
 
